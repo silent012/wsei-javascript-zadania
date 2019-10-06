86dc7fe35902e4217e57280aa7b11fc248d994de
Malinowski Andrzej <andrzej.malinowski@microsoft.wsei.edu.pl>
2019-10-06 17:14:11 +0200

    rozwiazanie zadania 1

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -1,3 +1,18 @@
-function distFromAvarage(param) {
-    for (let i= 1)
-}
+function distFromAvarage(array) {
+    for (let i = 0; i < array.length; i++) {
+        averageSum += array[i];
+    }
+
+    var averageValue = averageSum / array.length;
+    const outputArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+
+        outputArray.push(array[i]) - averageValue);
+    };
+
+    console.log(outputArray);
+    return outputArray;
+};
+
+distFromAvarage([1, 2, 3, 4, 5, 6, 7]);
