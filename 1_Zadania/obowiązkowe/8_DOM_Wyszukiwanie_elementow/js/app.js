9a4f509929320adc3b3add882a78b89d895361c8
Malinowski Andrzej <andrzej.malinowski@microsoft.wsei.edu.pl>
2019-12-08 17:26:09 +0100

    exercises

--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -3,4 +3,30 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
 
+    const title = document.querySelector('.title');
+
+    function getDataAnimation(element) {
+        let DataAnimation = element.getAttribute('data-animation');
+
+        return DataAnimation;
+    };
+
+    const home = document.getElementById('home');
+    const home2 = document.querySelector('#home');
+
+    const li1 = document.querySelector('li[data-direction]');
+
+    const block = document.querySelector('block';);
+
+    const first = document.querySelectorAll('nav li');
+    const second = document.querySelectorAll('div p');
+    const third = document.querySelectorAll('article div');
+
+    var article = document.querySelector('article-first');
+    console.log(article.querySelectorAll(log))
+
+////////////////////////////////////
+
+
+
 });
--- 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
+++ 1_Zadania/obowiązkowe/9_DOM_Element/js/app.js
@@ -9,4 +9,87 @@ document.addEventListener("DOMContentLoaded", function () {
     Poniżej napisz kod rozwiązujący zadania
      */
 
+     function getDataInfo(elements) {
+         var array = [];
+         for(let i = 0; i<elements.length; i++) {
+             array.push(elements[i].dataset.color);
+         }
+         console.log(array);
+     }
+
+     getDataInfo(links);
+
+     console.log(homeElement);
+     console.log(childElements);
+     console.log(banner);
+     console.log(blocks);
+     console.log(links);
+
+
+     for(let i = 0; i<childElements.length; i++) {
+         console.log(childElements[i].tagName, childElements[i].classList)
+     }
+
+     for(let i = 0; i<blocks.length; i++) {
+        console.log(blocks[i].tagName, blocks[i].classList)
+    }
+
+    for(let i = 0; i<links.length; i++) {
+        console.log(links[i].tagName, links[i].classList)
+    }
+
+
+    let blocksAll = document.querySelector('.blocks');
+
+    console.log(blocksAll.innerHTML);
+    console.log(blocksAll.outerHTML);
+
+    blocksAll.innerHTML = "new string"
+
+    const mainFooter = document.querySelector('#mainFooter');
+
+    function getId(element) {
+        return element.id;
+    }
+
+    const testId = getId(mainFooter);
+    console.log(testId);
+
+
+    function getTags(elements) {
+        var array1 = [];
+
+        for(i=0; i<elements.length; i++) {
+            array1.push(elements[i].tagName);
+        }
+        return array1;
+    }
+
+    const test1 = getTags(childElements);
+    console.log(test1);
+
+
+    function getClassInfo(element) {
+        var tab = [];
+        for(let i=0;i<element.classList.length;i++) {
+            tab.push(element.classList[i]);
+        }
+        return tab;
+    }
+
+    console.log(getClassInfo(banner));
+
+    //
+
+    var allLi = document.querySelectorAll('nav li')
+    function setDataDirection(elements) {
+        for(let i=0;i<elements.length; i++) {
+            if (!elements[i].getAttribute('data-direction') == "top") {
+                elements[i].setAttribute('data-direction', "top");
+            }
+        }
+    }
+
+    setDataDirection(allLi);
+
 });
